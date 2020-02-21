b3e283d55e6f6643112ce5409a060aa3b1554c81
kxdang <ikien.dang@gmail.com>
2020-02-21 01:13:52 -0500

    adding email validation with js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,31 @@ const makeRequestURI_CRYPTO = (type, tickers) => {
   });
 };
 
+//EMAIL VALIDATION
+
+const invalidEmail = document.getElementById("invalidEmail");
+
+const inputValidation = e => {
+  const input = e.target.value;
+
+  if (input == "") {
+    invalidEmail.setAttribute("class", "invalidNone");
+    return;
+  }
+
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)) {
+    console.log("yay email");
+    invalidEmail.setAttribute("class", "invalidNone");
+  } else {
+    invalidEmail.setAttribute("class", "invalid");
+    console.log("Not an email");
+  }
+};
+
+const emailInputForm = document.getElementById("email");
+
+emailInputForm.addEventListener("blur", inputValidation);
+
 //2020-02-19
 let currentDate = new Date();
 const yyyy = currentDate.getFullYear();
